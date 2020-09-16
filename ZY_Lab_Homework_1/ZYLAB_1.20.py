65513d2910de54ee91e274c3bb601560d3f704b7
KatherynBusch <70350739+KatherynBusch@users.noreply.github.com>
2020-09-16 09:51:33 -0500

    Add files via upload
    FIle for assignment 1.20

---
+++ ZY_Lab_Homework_1/ZYLAB_1.20.py
@@ -0,0 +1,15 @@
+userNum = int(input("Enter integer:\n"))
+userNumSquared = userNum * userNum
+userNumCubed = userNum * userNum * userNum
+print("You entered:", userNum)
+print(userNum, "squared is", userNumSquared)
+print("And", userNum, "cubed is", userNumCubed, "!!")
+userNum1 = int(input("Enter another integer:\n"))
+userNumSum = userNum + userNum1
+userNumProduct = userNum * userNum1
+print(userNum, "+", userNum1, "is", userNumSum)
+print(userNum, "*", userNum1, "is", userNumProduct)
+
+
+
+
