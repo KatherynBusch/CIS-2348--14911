93eae1e9c2e255bb778e7589649ba4aed7939fc3
KatherynBusch <70350739+KatherynBusch@users.noreply.github.com>
2020-11-02 23:25:31 -0600

    Add files via upload
    Zylab 10.11

---
+++ ZYLABS_3/ZYLAB_10.11.py
@@ -0,0 +1,30 @@
+# Katheryn Busch PSID: 1868948
+class FoodItem:
+    def __init__(self, name="None", fat=0.0, carb=0.0, protein=0.0):
+        self.name = name
+        self.fat = fat
+        self.carb = carb
+        self.protein = protein
+    def get_calories(self, servings):
+        calories = ((self.fat * 9) + (self.carb * 4) + (self.protein * 4)) * servings;
+        return calories
+    def print_info(self):
+        print('Nutritional information per serving of {}:'.format(self.name))
+        print('   Fat: {:.2f} g'.format(self.fat))
+        print('   Carbohydrates: {:.2f} g'.format(self.carb))
+        print('   Protein: {:.2f} g'.format(self.protein))
+if __name__ == "__main__":
+    food1 = FoodItem()
+    item = input()
+    fatamt = float(input())
+    carbamt = float(input())
+    proteinamt = float(input())
+    item2 = FoodItem(item, fatamt, carbamt, proteinamt)
+    servings = float(input())
+    food1.print_info()
+    print('Number of calories for {:.2f} serving(s): {:.2f}'.format(servings, food1.get_calories(servings)))
+
+    print()
+    item2.print_info()
+    print('Number of calories for {:.2f} serving(s): {:.2f}'.format(servings,                                                                   item2.get_calories(servings)))
+    item2.get_calories(servings)
