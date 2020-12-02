cf7932908e155b046e500d3e0cf1f6605e66d84a
KatherynBusch <70350739+KatherynBusch@users.noreply.github.com>
2020-12-02 11:42:33 -0600

    Add files via upload
    ZYLAB 14.11 which is descending selection sort with output during execution.

---
+++ ZYLAB_Homework_4/ZYLAB_14.11.py
@@ -0,0 +1,14 @@
+def selection_sort_descend_trace(lst):
+    for k in range(len(lst) - 1):
+        valueLarge = k
+        for w in range(k + 1, len(lst)):
+            if lst[w] > lst[valueLarge]:
+                valueLarge = w
+        lst[k], lst[valueLarge] = lst[valueLarge], lst[k]
+        for y in lst:
+            print(y, end=' ')
+        print()
+    return lst
+if __name__ == '__main__':
+    numbers = [int(y) for y in input().split()]
+    selection_sort_descend_trace(numbers)
